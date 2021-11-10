923f234a5556429db5d40e8a1bc56263202318e3
German David <germanmarinrolong@gmail.com>
2021-11-09 20:22:16 -0500

    rutas de login

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,24 @@
 const { Router } = require("express");
+const passport = require("passport");
+const { hideLoginAndRegister } = require("../lib/auth");
 
 const router = Router();
 
 router.get("/", (req, res) => {
     res.render("pages/index");
 });
+
+router.get("/login-form", hideLoginAndRegister, (req, res) => {
+    res.render("pages/users/login");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/profile",
+        failureRedirect: "/login-form",
+        failureFlash: true,
+    })
+);
 
 module.exports = router;
