27496452fc2a577b65b595481775895e2b1d7b22
German David <germanmarinrolong@gmail.com>
2021-11-12 21:34:54 -0500

    cambiar index page

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render("pages/index");
+    res.redirect("/login-form");
 });
 
 module.exports = router;
