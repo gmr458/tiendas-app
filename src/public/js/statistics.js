9b00b4a1c6e5c1492f903aaaf6cc9f804f8849b9
German David <germanmarinrolong@gmail.com>
2021-12-02 23:37:38 -0500

    cambios menores

--- src/public/js/statistics.js
+++ src/public/js/statistics.js
@@ -4,33 +4,52 @@ link.classList.add("active");
 fetch("/get-statistics-client-more-sales")
     .then((response) => response.json())
     .then((clients) => {
-    	let tbody = document.getElementById("rowsClientMoreSales");
-    	tbody.textContent = "";
+        let tbody = document.getElementById("rowsClientMoreSales");
+        tbody.textContent = "";
 
-        let i = 1;
-        
-        for (let client of clients) {
-        	let tr = document.createElement("tr");
+        if (clients.length > 0) {
+            let i = 1;
 
-            let tdNumber = document.createElement("td");
-            let textNumber = document.createTextNode(i);
-            tdNumber.appendChild(textNumber);
+            for (let client of clients) {
+                let tr = document.createElement("tr");
 
-        	let tdClientName = document.createElement("td");
-        	let textClientName = document.createTextNode(client.name_client);
-        	tdClientName.appendChild(textClientName);
+                let tdNumber = document.createElement("td");
+                let textNumber = document.createTextNode(i);
+                tdNumber.appendChild(textNumber);
 
-        	let tdQuantity = document.createElement("td");
-        	let textQuantity = document.createTextNode(client.quantity);
-        	tdQuantity.appendChild(textQuantity);
+                let tdClientName = document.createElement("td");
+                let textClientName = document.createTextNode(
+                    client.name_client
+                );
+                tdClientName.appendChild(textClientName);
 
-            tr.appendChild(tdNumber);
-        	tr.appendChild(tdClientName);
-        	tr.appendChild(tdQuantity);
+                let tdQuantity = document.createElement("td");
+                let textQuantity = document.createTextNode(client.quantity);
+                tdQuantity.appendChild(textQuantity);
 
-        	tbody.appendChild(tr);
+                tr.appendChild(tdNumber);
+                tr.appendChild(tdClientName);
+                tr.appendChild(tdQuantity);
 
-            i++;
+                tbody.appendChild(tr);
+
+                i++;
+            }
+        } else {
+            let tr = document.createElement("tr");
+
+            let tdMessage = document.createElement("td");
+            tdMessage.setAttribute("colspan", "3");
+            let h4Message = document.createElement("h4");
+            let textMessage = document.createTextNode(
+                "No hay datos suficientes para generar estadísticas"
+            );
+            h4Message.appendChild(textMessage);
+            tdMessage.appendChild(h4Message);
+
+            tr.appendChild(tdMessage);
+
+            tbody.appendChild(tr);
         }
     });
 
@@ -40,30 +59,51 @@ fetch("/get-statistics-neighborhood-more-sales")
         let tbody = document.getElementById("rowsNeighborhoodMoreSales");
         tbody.textContent = "";
 
-        let i = 1;
-        
-        for (let neighborhood of neighborhoods) {
+        if (neighborhoods.length > 0) {
+            let i = 1;
+
+            for (let neighborhood of neighborhoods) {
+                let tr = document.createElement("tr");
+
+                let tdNumber = document.createElement("td");
+                let textNumber = document.createTextNode(i);
+                tdNumber.appendChild(textNumber);
+
+                let tdNeighborhoodName = document.createElement("td");
+                let textNeighborhoodName = document.createTextNode(
+                    neighborhood.name_neighborhood
+                );
+                tdNeighborhoodName.appendChild(textNeighborhoodName);
+
+                let tdQuantity = document.createElement("td");
+                let textQuantity = document.createTextNode(
+                    neighborhood.quantity
+                );
+                tdQuantity.appendChild(textQuantity);
+
+                tr.appendChild(tdNumber);
+                tr.appendChild(tdNeighborhoodName);
+                tr.appendChild(tdQuantity);
+
+                tbody.appendChild(tr);
+
+                i++;
+            }
+        } else {
             let tr = document.createElement("tr");
 
-            let tdNumber = document.createElement("td");
-            let textNumber = document.createTextNode(i);
-            tdNumber.appendChild(textNumber);
+            let tdMessage = document.createElement("td");
+            tdMessage.setAttribute("colspan", "3");
+            let h4Message = document.createElement("h4");
+            let textMessage = document.createTextNode(
+                "No hay datos suficientes para generar estadísticas"
+            );
+            h4Message.appendChild(textMessage);
+            tdMessage.appendChild(h4Message);
 
-            let tdNeighborhoodName = document.createElement("td");
-            let textNeighborhoodName = document.createTextNode(neighborhood.name_neighborhood);
-            tdNeighborhoodName.appendChild(textNeighborhoodName);
-
-            let tdQuantity = document.createElement("td");
-            let textQuantity = document.createTextNode(neighborhood.quantity);
-            tdQuantity.appendChild(textQuantity);
-
-            tr.appendChild(tdNumber);
-            tr.appendChild(tdNeighborhoodName);
-            tr.appendChild(tdQuantity);
+            tr.appendChild(tdMessage);
 
             tbody.appendChild(tr);
-
-            i++;
         }
     });
 
@@ -73,29 +113,48 @@ fetch("/get-statistics-client-more-money")
         let tbody = document.getElementById("rowsClientMoreMoney");
         tbody.textContent = "";
 
-        let i = 1;
-        
-        for (let client of clients) {
+        if (clients.length > 0) {
+            let i = 1;
+
+            for (let client of clients) {
+                let tr = document.createElement("tr");
+
+                let tdNumber = document.createElement("td");
+                let textNumber = document.createTextNode(i);
+                tdNumber.appendChild(textNumber);
+
+                let tdClientName = document.createElement("td");
+                let textClientName = document.createTextNode(
+                    client.name_client
+                );
+                tdClientName.appendChild(textClientName);
+
+                let tdMoney = document.createElement("td");
+                let textMoney = document.createTextNode(`$${client.money}`);
+                tdMoney.appendChild(textMoney);
+
+                tr.appendChild(tdNumber);
+                tr.appendChild(tdClientName);
+                tr.appendChild(tdMoney);
+
+                tbody.appendChild(tr);
+
+                i++;
+            }
+        } else {
             let tr = document.createElement("tr");
 
-            let tdNumber = document.createElement("td");
-            let textNumber = document.createTextNode(i);
-            tdNumber.appendChild(textNumber);
+            let tdMessage = document.createElement("td");
+            tdMessage.setAttribute("colspan", "3");
+            let h4Message = document.createElement("h4");
+            let textMessage = document.createTextNode(
+                "No hay datos suficientes para generar estadísticas"
+            );
+            h4Message.appendChild(textMessage);
+            tdMessage.appendChild(h4Message);
 
-            let tdClientName = document.createElement("td");
-            let textClientName = document.createTextNode(client.name_client);
-            tdClientName.appendChild(textClientName);
-
-            let tdMoney = document.createElement("td");
-            let textMoney = document.createTextNode(`$${client.money}`);
-            tdMoney.appendChild(textMoney);
-
-            tr.appendChild(tdNumber);
-            tr.appendChild(tdClientName);
-            tr.appendChild(tdMoney);
+            tr.appendChild(tdMessage);
 
             tbody.appendChild(tr);
-
-            i++;
         }
     });
